aa90679214d7470355174bb6cae600bccd9348ea
Vichofeo <55749027+vichofeo@users.noreply.github.com>
2025-02-06 16:34:20 -0400

    ufam2_

--- src/services/aeb/params_cboxUfam.js
+++ src/services/aeb/params_cboxUfam.js
@@ -63,5 +63,60 @@ const PDEPENDENCIES = {
     },
     withInitial: true,
   },
+  ufam_air_genero: {
+    alias: 'ufam_air_genero',
+    campos: cmps,
+    ilogic: {
+      ufam_air: `SELECT eg.nombre_corto as ente_gestor, 'RRAME' AS grupo, t.gestion, COUNT(*) AS value
+                FROM tmp_rrame t, ae_institucion eg, ae_institucion i, al_departamento dpto
+                WHERE 
+                t.eg = i.institucion_root AND t.eess = i.institucion_id AND t.dpto = i.cod_dpto
+                AND i.institucion_root = eg.institucion_id 
+                AND i.cod_pais = dpto.cod_pais AND i.cod_dpto = dpto.cod_dpto
+                $w$
+                GROUP BY 1,2,3
+
+                UNION 
+
+                SELECT eg.nombre_corto as ente_gestor, 'INAS' AS grupo,  t.gestion, COUNT(*) AS value
+                FROM tmp_inas t, ae_institucion eg, ae_institucion i, al_departamento dpto
+                WHERE 
+                t.eg = i.institucion_root AND t.eess = i.institucion_id AND t.dpto = i.cod_dpto
+                AND i.institucion_root = eg.institucion_id 
+                AND i.cod_pais = dpto.cod_pais AND i.cod_dpto = dpto.cod_dpto
+                $w$
+                GROUP BY 1,2,3
+
+                UNION
+                
+                SELECT eg.nombre_corto as ente_gestor, 'AMES' AS grupo,  t.gestion, COUNT(*) AS value
+                FROM tmp_ames t, ae_institucion eg, ae_institucion i, al_departamento dpto
+                WHERE 
+                t.eg = i.institucion_root AND t.eess = i.institucion_id AND t.dpto = i.cod_dpto
+                AND i.institucion_root = eg.institucion_id 
+                AND i.cod_pais = dpto.cod_pais AND i.cod_dpto = dpto.cod_dpto
+                $w$
+                GROUP BY 1,2,3
+                ORDER BY 1,2,3
+                `,
+    },
+    referer: [],
+    primal: {
+      equivalencia: {
+        gestion: ['t.gestion', 't.gestion'],
+        periodo: [
+          "to_char(t.fecha_emision,'YYYY-MM')",
+          "to_char(t.fecha_emision,'YYYY-MM')",
+        ],
+        eg: ['i.institucion_root', 'i.institucion_root'],
+        dpto: ['i.cod_dpto', 'i.cod_dpto'],
+        eess: ['i.institucion_id', 'i.institucion_id'],
+      },
+      query: `SELECT $sa$`,
+      headers: [{}],
+      attributes: null,
+    },
+    withInitial: true,
+  },
 }
 module.exports = PDEPENDENCIES
