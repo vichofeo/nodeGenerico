926d1c20380ac8a6f082bd9692d5ec071ea4b651
Vichofeo <vichofeo@yahoo.com>
2025-06-06 16:32:29 -0400

    upfs16

--- src/services/upfs/parameters.js
+++ src/services/upfs/parameters.js
@@ -420,13 +420,8 @@ case ll.consumo_mensual  WHEN 0 THEN 0  else round ((ll.saldo_stock/ll.consumo_m
 
     /********************para reporte horizonrtal */
     rh_mensualn:{
-        table: `ae_institucion i, ae_institucion eg, al_departamento d, upf_registro r
-LEFT JOIN u_is_atributo as a1 ON (a1.atributo_id = r.concluido)`,
-        alias: 'rh_mensualn',
-        cardinalidad: "n",
-        linked: "eess",
-        campos: `d.cod_dpto, d.nombre_dpto, eg.nombre_corto, i.nombre_institucion,
-SUM(CASE WHEN substr(r.periodo,6,2)::DECIMAL =3 THEN 1 ELSE 0 END ) AS MARZO,
+        table: `ae_institucion i, ae_institucion eg, al_departamento d, upf_file_institucion_cnf cnf
+left join (SELECT r.institucion_id AS ints_idx,
 string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =1 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS ENE,
 string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =2 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS FEB,
 string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =3 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS MAR,
@@ -438,25 +433,115 @@ string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =8 THEN a1.atributo||'|'||a1
 string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =9 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS SEP,
 string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =10 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS OCT,
 string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =11 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS NOV,
-string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =12 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS DIC`,
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =12 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS DIC
+FROM  upf_registro r
+LEFT JOIN u_is_atributo as a1 ON (a1.atributo_id = r.concluido)
+WHERE 
+substr(r.periodo,1,4)=substr('|pd-0-pd|',1,4) AND r.file_tipo_id='|pd-1-pd|'
+GROUP BY 1) as tbl ON (tbl.ints_idx=cnf.institucion_id)`,
+        alias: 'rh_mensualn',
+        cardinalidad: "n",
+        linked: "eess",
+        campos: `d.cod_dpto as cdpto, d.nombre_dpto as dpto, eg.nombre_corto as eg, i.nombre_institucion as eess,
+tbl.*`,
 
         camposView: [
-        { value: "cod_liname", text: "Codigo" }, 
-        { value: "medicamento", text: "Medicamento" }, 
-        { value: "forma_farmaceutica", text: "Forma Farmacéutica" }, 
-        { value: "concentracion", text: "Concentración" },
-        { value: "clasificacion_atq", text: "Clasific. A.T.Q." },
-        { value: "uso", text: "Uso Restringido" },
-        { value: "aclaracion", text: "Aclaración de Particularidades" } 
+        { value: "dpto", text: "Departamento" }, 
+        { value: "eg", text: "Ente Gestor" }, 
+        { value: "eess", text: "Establecimiento" }
+
     ],
-        key: ['r.file_tipo_id'],
+        key: ['cnf.file_tipo_id'],        
+        keySession:{replaceKey:false, campo:'cnf.institucion_id'},
         paramDoms:[['r.periodo',0],['r.file_tipo_id',1]],
-        precondicion: ['r.institucion_id=i.institucion_id', 'i.institucion_root =  eg.institucion_id',
-            'i.cod_pais =  d.cod_pais',  'i.cod_dpto  =  d.cod_dpto',
-            `substr(r.periodo,0,5)=substr('|pd-0-pd|',0,5)`, `r.file_tipo_id='|pd-1-pd|'`
+        precondicion: ['cnf.institucion_id   = i.institucion_id', 'i.institucion_root =  eg.institucion_id',
+            'i.cod_pais =  d.cod_pais',  'i.cod_dpto  =  d.cod_dpto'           
              ],
-        groupOrder: ` GROUP BY 1,2,3,4
-                    ORDER BY 1,3,4 `,//null string    
+        groupOrder: ` ORDER BY 1,3,4 `,//null string    
+        update: [],
+        referer: [ ],
+
+    },
+    rh_semanaln:{
+        table: `ae_institucion i, ae_institucion eg, al_departamento d, upf_file_institucion_cnf cnf
+left join (SELECT r.institucion_id AS ints_idx,
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =1 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s01",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =2 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s02",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =3 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s03",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =4 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s04",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =5 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s05",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =6 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s06",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =7 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s07",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =8 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s08",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =9 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s09",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =10 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s10",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =11 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s11",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =12 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s12",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =13 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s13",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =14 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s14",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =15 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s15",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =16 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s16",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =17 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s17",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =18 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s18",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =19 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s19",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =20 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s20",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =21 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s21",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =22 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s22",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =23 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s23",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =24 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s24",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =25 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s25",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =26 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s26",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =27 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s27",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =28 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s28",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =29 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s29",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =30 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s30",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =31 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s31",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =32 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s32",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =33 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s33",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =34 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s34",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =35 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s35",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =36 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s36",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =37 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s37",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =38 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s38",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =39 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s39",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =40 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s40",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =41 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s41",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =42 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s42",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =43 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s43",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =44 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s44",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =45 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s45",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =46 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s46",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =47 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s47",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =48 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s48",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =49 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s49",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =50 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s50",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =51 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s51",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =52 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s52",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =53 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s53",
+string_agg(CASE WHEN substr(r.periodo,6,2)::DECIMAL =54 THEN a1.atributo||'|'||a1.color ELSE null END, '|' ) AS "s54"
+FROM  upf_registro r
+LEFT JOIN u_is_atributo as a1 ON (a1.atributo_id = r.concluido)
+WHERE 
+substr(r.periodo,1,4)=substr('|pd-0-pd|',1,4) AND r.file_tipo_id='|pd-1-pd|'
+GROUP BY 1) as tbl ON (tbl.ints_idx=cnf.institucion_id)`,
+        alias: 'rh_semanaln',
+        cardinalidad: "n",
+        linked: "eess",
+        campos: `d.cod_dpto as cdpto, d.nombre_dpto as dpto, eg.nombre_corto as eg, i.nombre_institucion as eess,
+tbl.*`,
+
+        camposView: [
+        { value: "dpto", text: "Departamento" }, 
+        { value: "eg", text: "Ente Gestor" }, 
+        { value: "eess", text: "Establecimiento" }
+
+    ],
+        key: ['cnf.file_tipo_id'],
+        paramDoms:[['r.periodo',0],['r.file_tipo_id',1]],
+        precondicion: ['cnf.institucion_id   = i.institucion_id', 'i.institucion_root =  eg.institucion_id',
+            'i.cod_pais =  d.cod_pais',  'i.cod_dpto  =  d.cod_dpto'           
+             ],
+        groupOrder: ` ORDER BY 1,3,4 `,//null string  ,//null string    
         update: [],
         referer: [ ],
 
